074799906a85dd56fd6e1adc990ce4ad31c9d8c9
Azerus <kevin.marous@gmail.com>
2017-08-22 11:37:51 +0200

    test lib

---
+++ test.js
@@ -0,0 +1,7 @@
+const fetch = require('.index.js');
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then(function(answer) {
+        console.log(answer);
+    }).catch(function(err) {
+        console.error(err);
+    });
